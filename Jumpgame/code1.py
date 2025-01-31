2abadc14659a20490f8707b6f802ae14b6b363f1
byungrock <obh7326@naver.com>
2025-02-01 00:53:40 +0900

    final

---
+++ Jumpgame/code1.py
@@ -0,0 +1,42 @@
+import pygame, sys
+from pygame.locals import * # pygame에 있는 모든기능을 사용
+
+# player 와 배경객체 추가하기
+
+def main():
+    # 게임 초기화 정보
+    pygame.init()
+    screen_width = 1800
+    screen_height = 900
+
+    # 제목 생성
+    pygame.display.set_caption('cat game')
+
+    # screen 객체
+    screen = pygame.display.set_mode((screen_width,screen_height))
+    bgImage = pygame.image.load('pictures/background.jpg')
+    bgImage = pygame.transform.scale(bgImage, (screen_width, screen_height))
+
+    # 플레이어 생성
+    # 가로 105, 세로 120 객체를 생성, 이후 객체의 왼쪽상단점의 위치를 다음 좌표로 설정
+    player = pygame.Rect( 10, 470, 105,120)
+    player_img = pygame.image.load('pictures/cat.png') # 이미지 할당
+    player_img = pygame.transform.scale(player_img, (105,120)) # 이미지 크기 조정
+
+    # 게임 속도
+    clock = pygame.time.Clock()
+
+    # 게임 실행에대해 처리되는 코드
+    while True:
+        dt = clock.tick(60) # 1초에 60번(hz) 업데이트
+        # 기능구현 1단계 : x 표시 누르면 시스템 종료하기
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+
+        screen.blit(bgImage, (0, 0))
+        screen.blit(player_img, player)
+        # 플레이어의 행동에대해 결과를 화면에 업데이트 하기위해 선언
+        pygame.display.update()
+main()
