ad0a3521947ae47321e7ce8a435b70f27e1f3686
byungrock <obh7326@naver.com>
2025-02-18 08:15:24 +0900

    loginf

--- Jumpgame/refactoring/login.py
+++ Jumpgame/refactoring/login.py
@@ -7,13 +7,14 @@ load_dotenv(dotenv_path="C:/Users/병록/cat_jump/Jumpgame/.venv/Scripts/.env")
 
 MONGO_URI = os.getenv("MONGO_URI")
 print('dd',MONGO_URI)
+
 # MongoDB 연결
 client = MongoClient(MONGO_URI)
 db = client["game_database"]
 collection = db["users"]
 
 # 데이터 삽입 테스트
-new_user = {"username": "player1", "password": "1234"}
-collection.insert_one(new_user)
+# new_user = {"username": "player1", "password": "1234"}
+# collection.insert_one(new_user)
 
 print("회원가입 성공!")
---
+++ Jumpgame/refactoring/loginscreen.py
@@ -0,0 +1,146 @@
+import pygame, configure, os
+
+pygame.init()
+
+# 만약 이미지 경로를 못읽는다면 shift + shift 를 누르고 Edit Configurations
+# 해당 working directory 경로를 C:/Users/병록/cat_jump/Jumpgame/refactoring 에서 C:/Users/병록/cat_jump/Jumpgame 으로 수정
+# 참고로 제가 현재 실행하고있는 파일의 위치는 C:/Users/병록/cat_jump/Jumpgame/refactoring/loginscreen.py 입니다.
+
+def scaled_id_pw():
+    # 부모 네모 원래 크기
+    parent_width = 966.07
+    parent_height = 772.81
+
+    # 자식 1 원래 정보
+    child1_x = 499.1966
+    child1_y = 358.209
+    child1_width = 229.6911
+    child1_height = 30.4363
+
+    # 자식 2 원래 정보
+    child2_x = 499.1966
+    child2_y = 410.0891
+    child2_width = 229.6911
+    child2_height = 30.4363
+
+    # 현재 화면 크기 (변화하는 값)
+    new_parent_width = configure.screen_width * 0.75
+    new_parent_height = configure.screen_height * 0.75
+
+    # 비율 적용한 새로운 값 계산
+    new_child1 = {
+        "x": (child1_x / parent_width) * new_parent_width * 0.724,
+        "y": (child1_y / parent_height) * new_parent_height * 0.858,
+        "width": (child1_width / parent_width) * new_parent_width,
+        "height": (child1_height / parent_height) * new_parent_height,
+    }
+
+    new_child2 = {
+        "x": (child2_x / parent_width) * new_parent_width * 0.724,
+        "y": (child2_y / parent_height) * new_parent_height * 0.875,
+        "width": (child2_width / parent_width) * new_parent_width,
+        "height": (child2_height / parent_height) * new_parent_height,
+    }
+
+    return new_child1, new_child2
+
+def loginscreen():
+    screen = pygame.display.set_mode((configure.screen_width * 0.75, configure.screen_height * 0.75))
+    pygame.display.set_caption('wendy game')
+
+    bgImage = pygame.image.load(os.path.join('pictures', 'loginscreen.png'))
+    bgImage = pygame.transform.scale(bgImage, (configure.screen_width * 0.75, configure.screen_height * 0.75))
+
+    clock = pygame.time.Clock()
+    WHITE = (255, 255, 255)
+    BLACK = (0, 0, 0)
+    BLUE = (0, 0, 255)
+
+    # 위치 및 크기 설정
+    id_x, id_y = scaled_id_pw()[0]['x'], scaled_id_pw()[0]['y']
+    id_width, id_height = scaled_id_pw()[0]['width'], scaled_id_pw()[0]['height']
+
+    pw_x, pw_y = scaled_id_pw()[1]['x'], scaled_id_pw()[1]['y']
+    pw_width, pw_height = scaled_id_pw()[1]['width'], scaled_id_pw()[1]['height']
+
+    input_id = pygame.Rect(id_x, id_y, id_width, id_height)
+    input_pw = pygame.Rect(pw_x, pw_y, pw_width, pw_height)
+
+    send_button_rect = pygame.Rect(200, 180, 100, 40)
+
+    font = pygame.font.Font(None, 36)
+    user_text_id = ""
+    user_text_pw = ""
+
+    active_id = False
+    active_pw = False
+
+    running = True
+    while running:
+        dt = clock.tick(60)
+
+        screen.blit(bgImage, (0, 0))
+
+        # 입력창 그리기
+        pygame.draw.rect(screen, WHITE, input_id, 2)
+        pygame.draw.rect(screen, BLACK, input_pw, 1)
+
+        # 전송 버튼 그리기
+        pygame.draw.rect(screen, BLUE, send_button_rect)
+        button_text = font.render("전송", True, WHITE)
+        screen.blit(button_text, (send_button_rect.x + 25, send_button_rect.y + 5))
+
+        # 입력된 텍스트 렌더링
+        text_surface_id = font.render(user_text_id, True, BLACK)
+        screen.blit(text_surface_id, (input_id.x + 10, input_id.y + 10))
+
+        text_surface_pw = font.render(user_text_pw, True, BLACK)
+        screen.blit(text_surface_pw, (input_pw.x + 10, input_pw.y + 10))
+
+        pygame.display.update()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if input_id.collidepoint(event.pos):
+                    active_id = True
+                    active_pw = False  # 아이디 입력창 클릭 시 비밀번호 입력창 비활성화
+                elif input_pw.collidepoint(event.pos):
+                    active_pw = True
+                    active_id = False  # 비밀번호 입력창 클릭 시 아이디 입력창 비활성화
+                else:
+                    active_id = False
+                    active_pw = False
+
+                if send_button_rect.collidepoint(event.pos):  # 전송 버튼 클릭
+                    print("입력된 아이디:", user_text_id)
+                    print("입력된 비밀번호:", user_text_pw)
+                    user_text_id = ""
+                    user_text_pw = ""
+
+            if event.type == pygame.KEYDOWN:
+                if active_id:
+                    if event.key == pygame.K_RETURN:
+                        print("입력된 아이디:", user_text_id)
+                        user_text_id = ""
+                    elif event.key == pygame.K_BACKSPACE:
+                        user_text_id = user_text_id[:-1]
+                    else:
+                        user_text_id += event.unicode
+
+                elif active_pw:
+                    if event.key == pygame.K_RETURN:
+                        print("입력된 비밀번호:", user_text_pw)
+                        user_text_pw = ""
+                    elif event.key == pygame.K_BACKSPACE:
+                        user_text_pw = user_text_pw[:-1]
+                    else:
+                        user_text_pw += event.unicode
+
+    pygame.display.update()
+
+loginscreen()
+
+
